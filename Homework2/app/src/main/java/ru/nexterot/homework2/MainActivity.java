8beb128ed2b49cc322f83db811aa61672df4d8fe
Alexander Barluka <nexterot@mail.ru>
2019-09-20 01:40:36 +0300

    Added minimal homework 2

--- Homework2/app/src/main/java/ru/nexterot/homework2/MainActivity.java
+++ Homework2/app/src/main/java/ru/nexterot/homework2/MainActivity.java
@@ -2,22 +2,43 @@ package ru.nexterot.homework2;
 
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+    private final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
+    private final String API_KEY = "3862f0bc7ec07174878a543c189bd2a0";
+    private final String MOSCOW_ID = "524901";
+    private final String SAINT_PETERSBURG_ID = "498817";
     private boolean lang;
+
+    private TextView weatherInfoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        weatherInfoTextView = findViewById(R.id.weather_info);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -28,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
         if (itemId == R.id.refresh) {
-            Toast.makeText(this, "Refresh clicked!", Toast.LENGTH_SHORT).show();
+            refresh();
             return true;
         } else if (itemId == R.id.switch_language) {
             switchLanguage();
@@ -42,12 +63,87 @@ public class MainActivity extends AppCompatActivity {
         Configuration config = res.getConfiguration();
         if (lang) {
             config.setLocale(new Locale("ru"));
-            Log.d("ggg", "true");
         } else {
             config.setLocale(new Locale("en"));
-            Log.d("ggg", "false");
         }
         lang = !lang;
         res.updateConfiguration(config, null);
+    }
+
+    public class requestInternet extends AsyncTask<URL, Void, String> {
+        @Override
+        protected String doInBackground(URL... urls) {
+            try {
+                String responseStr = getResponseFromHttpUrl(urls[0]);
+                Log.d("tag", responseStr);
+                String weatherStr = getWeatherFromJson(responseStr);
+                Log.d("tag", weatherStr);
+                return weatherStr;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            weatherInfoTextView.setText(String.format(getString(R.string.weather_description), s));
+            super.onPostExecute(s);
+        }
+    }
+
+    void refresh() {
+        Toast.makeText(this, "Refresh clicked!", Toast.LENGTH_SHORT).show();
+        download();
+    }
+
+    void download() {
+        Uri uri = Uri.parse(WEATHER_URL).buildUpon()
+                .appendQueryParameter("id", MOSCOW_ID)
+                .appendQueryParameter("appid", API_KEY)
+                .build();
+        URL url = null;
+        try {
+            url = new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        if (url != null) {
+            new requestInternet().execute(url);
+        }
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    String getWeatherFromJson(String jsonStr) {
+        try {
+            JSONObject weatherObj = new JSONObject(jsonStr);
+            JSONArray weather_predictions = weatherObj.getJSONArray("weather");
+            String weather = weather_predictions.getJSONObject(0).getString ("main");
+            return weather;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
